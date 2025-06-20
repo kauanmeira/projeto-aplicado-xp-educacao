615ca90d99086e13c941921a46e59707321c5724
kauan.paiva <kauan.paiva@attus.ai>
2025-06-20 20:59:03 -0300

    Commit inicial

---
+++ src/main/java/com/xp_educacao/projeto_aplicado/ProjetoAplicadoApplication.java
@@ -0,0 +1,13 @@
+package com.xp_educacao.projeto_aplicado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAplicadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xp_educacao/projeto_aplicado/ProjetoAplicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xp_educacao.projeto_aplicado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAplicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
